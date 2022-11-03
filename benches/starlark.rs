df9bbd5782146968755db1e37ae1a65a1a009c5f
Stephan Behnke <github@stephanbehnke.com>
2022-11-03 15:22:20 -0700

    fix

--- benches/starlark.rs
+++ benches/starlark.rs
@@ -16,10 +16,10 @@ fn functions(builder: &mut GlobalsBuilder) {
 
 const PROG: &'static str = r#"
 charset = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-def generate_string(len):
+def generate_string(size):
     data = []
-    for i in range(len):
-        data.append(charset[rand(len)])
+    for i in range(size):
+        data.append(charset[rand(len(charset))])
     return ' '.join(data)
 
 array = []
